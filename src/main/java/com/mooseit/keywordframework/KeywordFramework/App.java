5e924dc61f16e932ba645caad2ca85eda1fa5fe4
Chambers, Mustafa <mchambqa@gmail.com>
2020-05-14 00:32:47 -0400

    Initial Commits

---
+++ src/main/java/com/mooseit/keywordframework/KeywordFramework/App.java
@@ -0,0 +1,13 @@
+package com.mooseit.keywordframework.KeywordFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
